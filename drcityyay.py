1e667b1ce99ac94f481a0e5c850743be42b16f19
charudut shetty <cshetty@us.ibm.com>
2017-05-14 10:01:40 -0500

    modified code to accept just the CTA stopID without the appended .0

--- drcityyay.py
+++ drcityyay.py
@@ -1,37 +1,42 @@
 from flask import Flask, request
 from twilio import twiml
 import json
-import sys
- 
+
 app = Flask(__name__)
 
-def get_location_coordinates(stopID):
-	with open('CTA_BusStops.geojson') as data_file:    
-		data = json.load(data_file)
 
-	for feature in data['features']:
-    		selected_sys_stop = feature['properties']['SYSTEMSTOP']
-    		selected_sys_coordinates = feature['geometry']['coordinates']
-    		if (str(selected_sys_stop) == stopID):
-        		return selected_sys_coordinates 
- 
+def get_location_coordinates(stopID):
+    with open('CTA_BusStops.geojson') as data_file:
+        data = json.load(data_file)
+
+    for feature in data['features']:
+        selected_sys_stop = feature['properties']['SYSTEMSTOP']
+        selected_sys_coordinates = feature['geometry']['coordinates']
+        if str(selected_sys_stop) == stopID:
+            return selected_sys_coordinates
+
+
 @app.route('/sms', methods=['POST'])
 def sms():
     number = request.form['From']
     # user sends help text message including stop_id to (608) 514-1593
-    # for example, user sends "YELPCARE 1871" to (608) 514-1593
-    
+    # for example, user sends "DrCityYay 1871" to (608) 514-1593
+
     message_body = request.form['Body']
     # split the user_sent text based on white-space
-    
+
     keywords = message_body.split(" ")
-    stop_id = keywords[1]
-    print stop_id
+    stop_id_raw = keywords[1]
+    # print stop_id_raw
+    stop_id = stop_id_raw + '.0'
+    # print stop_id
+    # print "CTA rider send stopID: {}".format(stop_id)
     coordinates = get_location_coordinates(stop_id)
- 
+
     resp = twiml.Response()
     resp.message('Hello {}, based on your stopID your location is: {}'.format(number, coordinates))
     return str(resp)
- 
+
+
 if __name__ == '__main__':
     app.run()
